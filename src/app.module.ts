552ca5cbcaad396c2d263776ba8acf65ff438529
VISHWAr23 <vishwarajkumar05@gmail.com>
2026-04-15 12:42:34 +0530

    health check endpoint added

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,11 +2,13 @@ import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
 import { ChatModule } from './chat/chat.module';
 import { AuthModule } from './auth/auth.module';
+import { AppController } from './app.controller';
 
 const MONGODB_URI =
   process.env.MONGODB_URI || 'mongodb://localhost:27017/nest-chat';
 
 @Module({
+  controllers: [AppController],
   imports: [
     MongooseModule.forRoot(MONGODB_URI),
     AuthModule,
